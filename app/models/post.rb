73943ceee4c9e6bfe32cfa4b661cc207046fd17b
Abel Kalum <chumbabel@gmail.com>
2020-08-07 17:45:32 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,4 @@
 class Post < ActiveRecord::Base
-  def Post
-    it 'has a summary' do
-    post = Post.create!(title: "My title", description: "The post description")
-  expect(post.post_summary).to eq("My title - The post description")
-  end
-  
   def post_summary
     self.title + " - " + self.description
   end
