79bf57c6935e70e9d7abb56824c57ce47dd68905
Abel Kalum <chumbabel@gmail.com>
2020-08-07 17:41:44 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,6 @@ class Post < ActiveRecord::Base
   post = Post.create!(title: "My title", description: "The post description")
   expect(post.post_summary).to eq("My title - The post description")
   end
+  
+  
 end
